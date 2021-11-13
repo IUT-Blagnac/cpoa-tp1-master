016b84692ae888ebc616e08cecbc27315f557d4a
root <root@dev.chanouha.fr>
2021-11-14 00:30:05 +0100

    Pipeline génération graph

--- util/plantuml.sh
+++ util/plantuml.sh
@@ -5,21 +5,21 @@ MAINPATH='.'
 DOCLETPATH=$MAINPATH/doclet
 PUMLPATH=$MAINPATH/util
 
-ls
+ls 
 
 
-echo "Creating $UML..."
+echo "Creating $UML..." 
 
-javadoc	-private -quiet	-J-DdestinationFile=$UML -J-DcreatePackages=false -J-DshowPublicMethods=true -J-DshowPublicConstructors=false -J-DshowPublicFields=true -doclet de.mallox.doclet.PlantUMLDoclet -docletpath util/plantUmlDoclet.jar -sourcepath . src/main/java/*.java
+javadoc	-private -quiet	-J-DdestinationFile=$UML -J-DcreatePackages=false -J-DshowPublicMethods=true -J-DshowPublicConstructors=false -J-DshowPublicFields=true -doclet de.mallox.doclet.PlantUMLDoclet -docletpath util/plantUmlDoclet.jar -sourcepath . src/main/java/*.java 
 
-echo "Done creating plantUML model"
+echo "Done creating plantUML model" 
 
-cat $UML
+cat $UML 
 
-echo "Converting $UML to $TYPE..."
+echo "Converting $UML to $TYPE..." 
 #java -jar $PUMLPATH/plantuml.jar \
 java -jar util/plantuml-1.2021.14.jar \
   -config $PUMLPATH/config.cfg \
-  -t $TYPE $UML
-ret=$?
-echo "Done generating PNG from model $ret"
+  -t $TYPE $UML 
+ret=$? 
+echo "Done generating PNG from model $ret" 
