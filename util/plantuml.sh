2f5c56c95da37f5bc47008e59f38c289bf25e586
root <root@dev.chanouha.fr>
2021-11-13 23:42:23 +0100

    Pipeline génération graph

--- util/plantuml.sh
+++ util/plantuml.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
-UML='model.uml'
+UML=$(mktemp -d)'/model.uml'
 TYPE='PNG'
-MAINPATH='/Users/bruel/localdev/cpoa'
+MAINPATH='.'
 DOCLETPATH=$MAINPATH/doclet
 PUMLPATH=$MAINPATH/util
+
+
 echo "Creating $UML..."
-echo $DOCLETPATH
 
 javadoc \
--private \
--quiet \
--J-DdestinationFile=$UML \
--J-DcreatePackages=false \
--J-DshowPublicMethods=true \
--J-DshowPublicConstructors=false \
--J-DshowPublicFields=true \
--doclet de.mallox.doclet.PlantUMLDoclet -docletpath $DOCLETPATH/plantUmlDoclet.jar \
--sourcepath . src/Canard.java src/Colvert.java
+	-private \
+	-quiet \
+	-J-DdestinationFile=$UML \
+	-J-DcreatePackages=false \
+	-J-DshowPublicMethods=true \
+	-J-DshowPublicConstructors=false \
+	-J-DshowPublicFields=true \
+	-doclet de.mallox.doclet.PlantUMLDoclet -docletpath util/plantUmlDoclet.jar \
+	-sourcepath . src/main/java/*.java src/main/java/**/*.java
+
 
 echo "Done creating plantUML model"
 
-TYPE='png'
+cat $UML
+
 echo "Converting $UML to $TYPE..."
-java -jar $PUMLPATH/plantuml.jar \
+#java -jar $PUMLPATH/plantuml.jar \
+java -jar util/plantuml-1.2021.14.jar \
   -config $PUMLPATH/config.cfg \
   -t $TYPE $UML
-echo "Done generating PNG from model"
+ret=$?
+echo "Done generating PNG from model $ret"
